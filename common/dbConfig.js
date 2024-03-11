b4abfbd8196854845acc560d7ec9705473d2582d
neerajy818285 <neeraj818285@gmail.com>
2024-03-11 16:05:14 +0530

    first commit

---
+++ common/dbConfig.js
@@ -0,0 +1,6 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient;
+const dbName = "nodejs-Task-3"
+const url = "neerajy818285";
+
+module.exports={url,mongodb,MongoClient}
